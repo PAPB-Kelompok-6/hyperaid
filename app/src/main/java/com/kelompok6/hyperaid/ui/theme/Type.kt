3aeed58c7c9768625ebd9ff106afb843ed9155db
Prigel Kusumawardani <prigel17@gmail.com>
2025-10-28 22:15:18 +0700

    Typography updated

--- app/src/main/java/com/kelompok6/hyperaid/ui/theme/Type.kt
+++ app/src/main/java/com/kelompok6/hyperaid/ui/theme/Type.kt
@@ -1,34 +1,86 @@
 package com.kelompok6.hyperaid.ui.theme
 
+import com.kelompok6.hyperaid.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    // Titles (H1..H7)
+    displayLarge = TextStyle(            // H1 - Semibold / 30px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = TextStyle(           // H2 - Semibold / 24px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    displaySmall = TextStyle(            // H3 - Semibold / 18px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp
+    ),
+    headlineSmall = TextStyle(           // H4 - Semibold / 16px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(              // H5 - Semibold / 14px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+    titleMedium = TextStyle(             // H6 - Regular / 14px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    titleSmall = TextStyle(              // H7 - Semibold / 12px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp
+    ),
+
+    // Subhead & Body
+    headlineLarge = TextStyle(           // Subhead 1 - Regular / 20px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    headlineMedium = TextStyle(          // Subhead 2 - Regular / 18px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+    bodyLarge = TextStyle(               // Paragraph 1 - Regular / 18px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+    bodyMedium = TextStyle(              // Paragraph 2 - Regular / 16px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodySmall = TextStyle(               // Paragraph 3 - Medium / 14px
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp
+    ),
+    labelSmall = TextStyle(              // Paragraph 4 - Regular / 12px
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     )
-    */
 )
