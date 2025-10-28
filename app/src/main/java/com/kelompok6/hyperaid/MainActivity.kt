f25fdc6d0b3e38e2a7c4fec85786f8d803f64b9f
Prigel Kusumawardani <prigel17@gmail.com>
2025-10-28 21:57:52 +0700

    assets module added

--- app/src/main/java/com/kelompok6/hyperaid/MainActivity.kt
+++ app/src/main/java/com/kelompok6/hyperaid/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.kelompok6.hyperaid.ui.navigation.RootNavGraph
 import com.kelompok6.hyperaid.ui.theme.HyperAidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HyperAidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                RootNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HyperAidTheme {
-        Greeting("Android")
     }
 }
