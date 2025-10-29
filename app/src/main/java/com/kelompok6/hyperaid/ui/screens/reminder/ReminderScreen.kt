9876bf2dd6fd17476629b268a9d7eeb53c88c2be
rafinran <rafinran@gmail.com>
2025-10-29 11:53:39 +0700

    Gawe reminder kosong

--- app/src/main/java/com/kelompok6/hyperaid/ui/screens/reminder/ReminderScreen.kt
+++ app/src/main/java/com/kelompok6/hyperaid/ui/screens/reminder/ReminderScreen.kt
@@ -1,18 +1,81 @@
 package com.kelompok6.hyperaid.ui.screens.reminder
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.kelompok6.hyperaid.R
 
-@Suppress("unused", "UNUSED_PARAMETER")
 @Composable
 fun ReminderScreen(navController: NavHostController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Reminder", style = MaterialTheme.typography.headlineSmall)
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Text(text = "Reminder", fontSize = 24.sp, fontWeight = FontWeight.Bold, fontFamily = FontFamily.SansSerif)
+
+            Image(
+                painter = painterResource( R.drawable.jam_merah),
+                contentDescription = "Alarm",
+                modifier = Modifier.size(400.dp)
+            )
+
+            Text(text = "A little reminder for you!", fontSize = 18.sp, fontWeight = FontWeight.Bold, fontFamily = FontFamily.SansSerif)
+
+            Spacer(Modifier.height(20.dp))
+
+            Text(text = "Never miss a health check-up again! Use this feature to keep track of all your appointments.",
+                Modifier.padding(horizontal = 40.dp), lineHeight = 22.5.sp, fontSize = 14.sp, fontFamily = FontFamily.SansSerif, textAlign = TextAlign.Center)
+        }
+
+        // ➕ FAB di kanan bawah
+        FloatingActionButton(
+//            onClick = { showSheet = true },
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(10.dp)
+                .clip(CircleShape),
+            containerColor = Color.Black
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "Add Reminder", tint = Color.White)
+        }
     }
 }
