32f29f132e8e43fea33ebf56601bb56be48ebd24
Yoga Raditya <radityanalaa@gmail.com>
2025-10-29 12:34:26 +0700

    feat: add register screen

--- app/src/main/java/com/kelompok6/hyperaid/ui/screens/start/RegisterScreen.kt
+++ app/src/main/java/com/kelompok6/hyperaid/ui/screens/start/RegisterScreen.kt
@@ -1,17 +1,158 @@
 package com.kelompok6.hyperaid.ui.screens.start
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.kelompok6.hyperaid.R
 
 @Composable
 fun RegisterScreen(navController: NavHostController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Register", style = MaterialTheme.typography.headlineSmall)
+    val scrollState = rememberScrollState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp, 50.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.verticalScroll(scrollState),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.hyperaid),
+                contentDescription = "Hyperaid logo",
+                modifier = Modifier.widthIn(max = 175.dp)
+            )
+
+            Text(
+                text = "Register to your account and manage your health right away!",
+                modifier = Modifier
+                    .padding(20.dp, 10.dp),
+                style = MaterialTheme.typography.bodySmall,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Center
+            )
+
+            RegisterField(navController)
+            OrDivider()
+            LoginOAuth(navController)
+
+            Spacer(modifier = Modifier.padding(0.dp, 25.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                Label("Have an account?")
+                Text(
+                    modifier = Modifier.clickable() {
+                        navController.navigate("Login")
+                    },
+                    text = "Login",
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RegisterField(navController: NavController) {
+    var fullName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        OutlinedTextField(
+            value = fullName,
+            onValueChange = { fullName = it },
+            label = { Label("Full Name") },
+            shape = RoundedCornerShape(25f),
+            textStyle = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Label("Email") },
+            shape = RoundedCornerShape(25f),
+            textStyle = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Label("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            shape = RoundedCornerShape(25f),
+            textStyle = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Label("Confirm Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            shape = RoundedCornerShape(25f),
+            textStyle = MaterialTheme.typography.bodySmall,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 10.dp))
+
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ),
+            shape = RoundedCornerShape(25f),
+            onClick = { navController.navigate("Login") },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Label("REGISTER")
+        }
     }
 }
