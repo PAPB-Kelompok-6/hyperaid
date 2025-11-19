a6dbc54f6e1b4de24537756ba1c485299c108f6e
revnaufl <revo.naufal@gmail.com>
2025-11-19 11:49:31 +0700

    comment Preview in NutritrackScreen

--- app/src/main/java/com/kelompok6/hyperaid/ui/screens/fitsync/nutritrack/NutritrackScreen.kt
+++ app/src/main/java/com/kelompok6/hyperaid/ui/screens/fitsync/nutritrack/NutritrackScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun NutriTrackScreen(navController: NavController) {
     var selectedTab by remember { mutableStateOf("NutriTrack") }
